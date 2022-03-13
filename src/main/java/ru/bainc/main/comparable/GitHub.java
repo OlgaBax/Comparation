7e20bad11ad33b8a6c65b14e0bcf3fad7e69462d
Babushkina Olga <ba80@mail.ru>
2022-03-13 12:55:29 +0300

    Изменила Test1.java и добавила GitHub.java

--- src/main/java/ru/bainc/main/comparable/GitHub.java
+++ src/main/java/ru/bainc/main/comparable/GitHub.java
@@ -3,5 +3,7 @@ package ru.bainc.main.comparable;
 public class GitHub {
     public static void main(String[] args) {
         System.out.println("I like gitHub");
+        System.out.println("Good day");
+
     }
 }
