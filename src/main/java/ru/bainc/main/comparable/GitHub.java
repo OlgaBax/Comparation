963465f0fd44d7ab3099f33a32cbe4bb1c778227
Babushkina Olga <ba80@mail.ru>
2022-03-14 09:56:28 +0300

    Изменила GitHub.java

--- src/main/java/ru/bainc/main/comparable/GitHub.java
+++ src/main/java/ru/bainc/main/comparable/GitHub.java
@@ -3,7 +3,6 @@ package ru.bainc.main.comparable;
 public class GitHub {
     public static void main(String[] args) {
         System.out.println("I like gitHub");
-        System.out.println("Good day");
 
     }
 }
